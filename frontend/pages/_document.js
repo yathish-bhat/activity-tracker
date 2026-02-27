377ee6fbcf70dffd5d030de3a11dd7a822e495e3
Yathish H Bhat <108626953+yathish-bhat@users.noreply.github.com>
2026-02-27 21:53:07 +0530

    Add Google Fonts link to _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link
+              href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500&display=swap"
+              rel="stylesheet"
+        />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/icon-192.png"></link>
